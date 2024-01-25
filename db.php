373421f54bf9f7066775089054e44b5e9ad02f24
Itachi23-san <69537376+Itachi23-san@users.noreply.github.com>
2024-01-25 20:41:55 +0300

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $host = 'localhost'; // имя хоста
-  $user = 'root';      // имя пользователя
-  $pass = '';          // пароль
-  $name = 'ws';      // имя базы данных
-
-  $link = mysqli_connect($host, $user, $pass, $name);
-  
