2af7f55d605b08874958d0fc4367ab10edc4abad
Itachi23-san <69537376+Itachi23-san@users.noreply.github.com>
2024-01-29 12:07:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
 class Arifm {
 
-    constructor(num1, num2, degree, modul) {
+    constructor(num1, num2, modul) {
         this.num1 = num1;
         this.num2 = num2;
-        this.degree = degree;
         this.modul = modul;
     }
 
@@ -15,15 +14,14 @@ class Arifm {
         return `Произведение x и y по модулю ${this.modul}: ` + (this.num1 * this.num2) % this.modul;
     }
     Degree() {
-        return `Число х в степени z по модулю ${this.modul}: ` + (this.num1 ** this.degree) % this.modul + " " + `Число у в степени z по модулю ${this.modul}: ` + (this.num2 ** this.degree) % this.modul;
+        return `Число х в степени y по модулю ${this.modul}: ` + (this.num1 ** this.num2) % this.modul;
     }
 }
 
 var x = prompt("Введите число x:");
 var y = prompt("Введите число y:");
-var z = prompt("Введите степень:");
-var modul = prompt("Введите модуль:");
+var z = prompt("Введите модуль:");
 
-var numbers = new Arifm(x, y, z, modul);
+var numbers = new Arifm(x, y, z);
 
 document.writeln(numbers.Summa() + "<br>" + numbers.Mult() + "<br>" + numbers.Degree());
